26a94961f5ea8713d414b39c7280fdea28288778
claratorres1414 <claratorres12345679@gmail.com>
2025-10-08 10:31:48 -0300

    #2 - foi adicionada uma página inicial que é a home_ui, essa página tem o intuito de redirecionar para o cadastro.

---
+++ client/views/home_ui.py
@@ -0,0 +1,22 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
+
+
+class HomeUi(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel("Bem-vindo ao UrChat!")
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setFont(QFont("Arial", 20, QFont.Weight.Bold))
+        self.cadastro_label = QLabel("É novo por aqui? Faça o seu cadastro clicando nesse botão:")
+        self.cadastro_btn = QPushButton("Cadastrar")
+
+        layout.addWidget(self.label)
+        layout.addWidget(self.cadastro_label)
+        layout.addWidget(self.cadastro_btn)
+
+        self.setLayout(layout)
