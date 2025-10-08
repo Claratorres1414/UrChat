9b75702c04cbd093624b9c627f01429c5603e62a
claratorres1414 <claratorres12345679@gmail.com>
2025-10-08 10:32:35 -0300

    #2 - ajustei o cadastro_controller para requisitar uma main_window na sua inicialização, acrescentei um botão com evento de click que volta para a página inicial e criei o tp_home que faz esse trabalho de voltar para a home;

--- client/controllers/cadastro_controller.py
+++ client/controllers/cadastro_controller.py
@@ -2,10 +2,13 @@ from PyQt6.QtWidgets import QMessageBox
 from client.services.api_service import ApiService
 
 class CadastroController:
-    def __init__(self, ui, api_service: ApiService):
+    def __init__(self, ui, main_window, api_service: ApiService):
         self.ui = ui
+        self.main_window = main_window
         self.api_service = api_service
+
         self.ui.cadastrar_btn.clicked.connect(self.cadastrar_usuario)
+        self.ui.voltar_btn.clicked.connect(self.tp_home)
     def cadastrar_usuario(self):
         username = self.ui.username_input.text().strip()
         password = self.ui.password_input.text().strip()
@@ -25,3 +28,6 @@ class CadastroController:
             self.ui.password_input.clear()
         else:
             QMessageBox.critical(self.ui, "Erro", result["error"])
+
+    def tp_home(self):
+        self.main_window.mostrar_tela("home")
