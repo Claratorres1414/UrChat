33d88c0d751cfe822a829bca06d719c1c3f2f9fe
claratorres1414 <claratorres12345679@gmail.com>
2025-10-08 11:59:09 -0300

    #3 - Adicionado o botão de entrar no home, e adicionado o método tp_login no home_controller para que o usuário possa acessar a página de login.

--- client/controllers/home_controller.py
+++ client/controllers/home_controller.py
@@ -4,6 +4,10 @@ class HomeController:
         self.main_window = main_window
 
         self.ui.cadastro_btn.clicked.connect(self.tp_cadastro)
+        self.ui.login_btn.clicked.connect(self.tp_login)
 
     def tp_cadastro(self):
         self.main_window.mostrar_tela("cadastro")
+
+    def tp_login(self):
+        self.main_window.mostrar_tela("login")
--- client/views/home_ui.py
+++ client/views/home_ui.py
@@ -14,9 +14,11 @@ class HomeUi(QWidget):
         self.label.setFont(QFont("Arial", 20, QFont.Weight.Bold))
         self.cadastro_label = QLabel("É novo por aqui? Faça o seu cadastro clicando nesse botão:")
         self.cadastro_btn = QPushButton("Cadastrar")
+        self.login_btn = QPushButton("Entrar")
 
         layout.addWidget(self.label)
         layout.addWidget(self.cadastro_label)
         layout.addWidget(self.cadastro_btn)
+        layout.addWidget(self.login_btn)
 
         self.setLayout(layout)
