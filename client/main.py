81ec86f472c7dbfb34f814f4c8941d7af16eb4bd
claratorres1414 <claratorres12345679@gmail.com>
2025-10-08 10:33:56 -0300

    #2 - foi adicionado um init na main que tem a funcionalidade de setar o titúlo da janela ao iniciar o programa para UrChat, ajusta a forma de servir páginas para uma stack, ou seja: a navegação é feita nessa pilha, a saída de uma página para outra ocorre nela, no init também está sendo armazenada a url base do servidor, ele inicializa as classes de Ui e adiciona elas na pilha, depois seta um nome para as páginas e faz uso dos controladores para navegar entre as páginas e faz com que a primeira tela apresentada seja a home.

--- client/main.py
+++ client/main.py
@@ -1,17 +1,43 @@
 import sys
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
+
+from client.controllers.home_controller import HomeController
+from client.views.home_ui import HomeUi
 from views.cadastro_ui import CadastroUi
 from controllers.cadastro_controller import CadastroController
 from services.api_service import ApiService
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("UrChat")
+
+        self.stack = QStackedWidget()
+        self.setCentralWidget(self.stack)
+
+        self.api_service = ApiService(base_url="http://127.0.0.1:8000")
+
+        self.home_ui = HomeUi()
+        self.cadastro_ui = CadastroUi()
+
+        self.stack.addWidget(self.home_ui)
+        self.stack.addWidget(self.cadastro_ui)
+
+        self.telas = {
+            "home": self.stack.indexOf(self.home_ui),
+            "cadastro": self.stack.indexOf(self.cadastro_ui)
+        }
+
+        self.home_controller = HomeController(self.home_ui, self)
+        self.cadastro_controller = CadastroController(self.cadastro_ui, self, self.api_service)
+
+        self.mostrar_tela("home")
+
+    def mostrar_tela(self, name):
+        self.stack.setCurrentIndex(self.telas[name])
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    ui = CadastroUi()
-
-    api_service = ApiService("http://127.0.0.1:8000")
-
-    controller = CadastroController(ui, api_service)
-
-    ui.show()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec())
