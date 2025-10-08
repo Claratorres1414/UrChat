1c1d4d118b9f4d7ce126c87c95b4481477a3aa23
claratorres1414 <claratorres12345679@gmail.com>
2025-10-08 10:34:57 -0300

    #2 - foi feita uma atualização na estrutura do cadastro_ui para deixar mais organizado e acrescentar o botão de voltar.

--- client/views/cadastro_ui.py
+++ client/views/cadastro_ui.py
@@ -3,22 +3,25 @@ from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
 class CadastroUi(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("- Cadastro -")
-        self.setGeometry(100, 100, 300, 200)
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+        layout = QVBoxLayout()
 
         self.username_label = QLabel("Nome de usuário: ")
         self.username_input = QLineEdit()
-        self.layout.addWidget(self.username_label)
-        self.layout.addWidget(self.username_input)
 
         self.password_label = QLabel("Senha: ")
         self.password_input = QLineEdit()
         self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
-        self.layout.addWidget(self.password_label)
-        self.layout.addWidget(self.password_input)
 
         self.cadastrar_btn = QPushButton("Cadastrar")
-        self.layout.addWidget(self.cadastrar_btn)
+        self.voltar_btn = QPushButton("Voltar")
+
+        layout.addWidget(QLabel("Cadastro de Usuário"))
+        layout.addWidget(self.username_label)
+        layout.addWidget(self.username_input)
+        layout.addWidget(self.password_label)
+        layout.addWidget(self.password_input)
+        layout.addWidget(self.cadastrar_btn)
+        layout.addWidget(self.voltar_btn)
+
+        self.setLayout(layout)
