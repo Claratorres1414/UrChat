bcd68f0333630ca16a3d2be06b72d396d13744d0
claratorres1414 <claratorres12345679@gmail.com>
2025-10-08 11:32:20 -0300

    #3 - Criada a estrutura inicial do login_controller.py com um construtor que vai passar o ui, o main padrão e o api_service;  também foi foi criada a função entrar_usuario que vai pegar os dados informados de username e senha lá no ui, vê se os campos estão preenchidos (se sim, segue. Se não, abre um aviso de erro e para), seguindo ele testa a resposta e em caso de sucesso mostra um popup de sucesso, em caso de erro lança um popup de erro.

---
+++ client/controllers/login_controller.py
@@ -0,0 +1,32 @@
+from PyQt6.QtWidgets import QMessageBox
+
+from client.services.api_service import ApiService
+
+
+class LoginController:
+    def __init__(self, ui, main_window, api_service: ApiService):
+        self.ui = ui
+        self.main_window = main_window
+        self.api_service = api_service
+
+        self.ui.cadastrar_btn.clicked.connect(self.entrar_usuario)
+        self.ui.voltar_btn.clicked.connect(self.tp_home)
+
+    def entrar_usuario(self):
+        username = self.ui.username_input.text().strip()
+        password = self.ui.password_input.text().strip()
+
+        if not username or not password:
+            QMessageBox.warning(self.ui, "Erro", "Preencha todos campos!")
+            return
+
+        result = self.api_service.login_user(username, password)
+        if result["success"]:
+            QMessageBox.information(self.ui, "Sucesso", result["message"])
+            self.ui.username_input.clear()
+            self.ui.password_input.clear()
+        else:
+            QMessageBox.warning(self.ui, "Erro", result["error"])
+
+    def tp_home(self):
+        self.main_window.mostrar_tela("home")
