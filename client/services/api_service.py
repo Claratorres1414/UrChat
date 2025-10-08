aadc4338ce768e86706199f752408ead55b160e5
claratorres1414 <claratorres12345679@gmail.com>
2025-10-08 11:43:52 -0300

    #3 - Adicionada a funcao de login no api_service responsável por tentar colocar as informações de username e senha no formato json esperado pelo servidor, enviar a requisição, pegar sua resposta e dar um retorno para o controller de sucesso ou insucesso.

--- client/services/api_service.py
+++ client/services/api_service.py
@@ -16,3 +16,15 @@ class ApiService:
                 return {"success": False, "detail": detail}
         except RequestException as e:
             return {"success": False, "detail": str(e)}
+
+    def login_user(self, username: str, password: str) -> dict:
+        try:
+            payload = {"username": username, "password": password}
+            response = requests.post(f"{self.base_url}/auth/login", json=payload)
+            if response.status_code in (200, 201):
+                return {"success": True, "data": response.json()}
+            else:
+                detail = response.json().get("detail", "Erro desconhecido")
+                return {"success": False, "detail": detail}
+        except RequestException as e:
+            return {"success": False, "detail": str(e)}
