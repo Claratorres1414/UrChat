034a4b01340d8b4906664f53d2ef0e1dc5f276f5
claratorres1414 <claratorres12345679@gmail.com>
2025-10-12 16:40:36 -0300

    #8 - adicionado o evento de enviar mensagem ao botão de enviar na página de chat; declarada a ativação do message_callback com o método on_message_received para o programa entenda que uma mensagem chegou; criado o método send_message que recebe a mensagem digitada pelo usuário e a envia pelo ws para o servidor.

--- client/controllers/conversation_chat_controller.py
+++ client/controllers/conversation_chat_controller.py
@@ -1,5 +1,9 @@
+from PyQt6.QtCore import QObject, pyqtSignal
+
 from client.services.api_service import ApiService
 
+class ChatSignals(QObject):
+    messageReceived = pyqtSignal(str)
 
 class ConversationChatController:
     def __init__(self, ui, main_window, api_service: ApiService, contact_username: str):
@@ -10,7 +14,26 @@ class ConversationChatController:
 
         self.ui.set_contact_name(contact_username)
 
+        self.ui.send_button.clicked.connect(self.send_message)
         self.ui.voltar_btn.clicked.connect(self.tp_main_chat)
+
+        self.api_service.set_message_callback(self.on_message_received)
+
+    def send_message(self):
+        msg = self.ui.message_input.text().strip()
+        if not msg:
+            return
+
+        self.ui.message_input.clear()
+
+        try:
+            if self.api_service.ws:
+                self.api_service.ws.send(msg)
+        except Exception as e:
+            print("Erro ao enviar mensagem: ", e)
+
+    def on_message_received(self, msg):
+        self.ui.chat_display.append(msg)
 
     def tp_main_chat(self):
         self.main_window.mostrar_tela("main_chat")
