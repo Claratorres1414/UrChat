b19268beba5b4c03dc1f65f40459578ea3368dcb
claratorres1414 <claratorres12345679@gmail.com>
2025-10-12 08:09:32 -0300

    #6 - atualização no main_chat_ui para acrescentar a lista de contatos ao layout e mudar o título da página para Chat

--- client/views/main_chat_ui.py
+++ client/views/main_chat_ui.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QLabel, QWidget, QPushButton
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QLabel, QWidget, QPushButton, QListWidget
 
 
 class MainChatUI(QWidget):
@@ -7,10 +7,12 @@ class MainChatUI(QWidget):
 
         layout = QVBoxLayout()
 
-        self.label = QLabel("Chat em desenvolvimento...")
+        self.label = QLabel("Chat")
+        self.contacts_list = QListWidget()
         self.sair_btn = QPushButton("Sair")
 
         layout.addWidget(self.label)
+        layout.addWidget(self.contacts_list)
         layout.addWidget(self.sair_btn)
 
         self.setLayout(layout)
