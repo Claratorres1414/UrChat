c2246ce3dacd579c7782be4df995536a3f42b996
claratorres1414 <claratorres12345679@gmail.com>
2025-10-12 08:08:34 -0300

    #6 - atualização no construtor da classe para receber o ApiService ao inicializar a classe, implementado também o novo método load_contacts que chama a funcção list_contacts do api_service, limpa a lista atual, testa se a resposta do api_service não está vazia (em caso de estar a função põe "nenhum contato encontrado" na lista e dá um return vazio), em caso de prosseguir a lista cai em um for que vai separar os dicionários e pegar as informações relevantes para exibição.

--- client/controllers/main_chat_controller.py
+++ client/controllers/main_chat_controller.py
@@ -1,9 +1,46 @@
+from PyQt6.QtCore import QTimer
+from PyQt6.QtWidgets import QListWidgetItem
+from requests import RequestException
+
+from client.services.api_service import ApiService
+
+
 class MainChatController:
-    def __init__(self, ui, main_window):
+    def __init__(self, ui, main_window, api_service: ApiService):
         self.ui = ui
         self.main_window = main_window
+        self.api_service = api_service
 
         self.ui.sair_btn.clicked.connect(self.tp_home)
+
+        self.load_contacts()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.load_contacts)
+        self.timer.start(1000) #Se aumentar o fluxo de clientes, aumentar tempo
+
+    def load_contacts(self):
+        try:
+            contacts = self.api_service.list_contacts()
+            print("contatos recebidos", contacts)
+
+            self.ui.contacts_list.clear()
+
+            if not contacts:
+                self.ui.contacts_list.addItem(QListWidgetItem("Nenhum contato encontrado."))
+                return
+
+            for contact in contacts:
+                if contact["status"] == "online":
+                    item_text = f"{contact["username"]} ({contact["status"]})"
+                else:
+                    item_text = f"{contact["username"]} ({contact["status"]} - {contact["last_seen"]})"
+
+                item = QListWidgetItem(item_text)
+                self.ui.contacts_list.addItem(item)
+
+        except RequestException as e:
+            print(f"Erro ao carregar contatos: {e}")
 
     def tp_home(self):
         self.main_window.mostrar_tela("home")
